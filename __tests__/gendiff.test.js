5c379027e11b739296a1337fdcb8853036c55091
Alexander Sinitsin <workbigbox@yandex.ru>
2021-06-17 14:08:00 +0700

    add task 1

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,20 @@
+import gendiff from '../src/main.js';
+
+test('getdiff', () => {
+  const file1 = {
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+  }
+  
+  const file2 = {
+    "timeout": 20,
+    "verbose": true,
+    "host": "hexlet.io"
+  }
+  
+  const result = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+
+  expect(gendiff('../frontend-project-lvl2/bin/file1.json', '../frontend-project-lvl2/bin/file2.json')).toEqual(result); 
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import gendiff from '../src/main.js'
+
 const program = new Command();
 
 program
+.arguments('<filepath1> <filepath2>')
 .description('Compares two configuration files and shows a difference.')
-.version('0.0.1', '-V, --version', 'output the current version');
+.version('0.0.1', '-V, --version', 'output the current version')
+.option('-f, --format [type]', 'output format')
+.action(gendiff);
 
-program.parse();
-
-const options = program.opts();
+program.parse(process.argv);
