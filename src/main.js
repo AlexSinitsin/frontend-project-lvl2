1852af07ec96e6acf324f09b4cce3461b1deb045
Alexander Sinitsin <workbigbox@yandex.ru>
2021-06-18 14:49:11 +0700

    test

--- src/main.js
+++ src/main.js
@@ -5,13 +5,7 @@ import _ from 'lodash';
 const gendiff = (filepath1, filepath2) => {
   const path1 = path.resolve(filepath1);
   console.log(path1 + '&');
-  const path2 = path.resolve(filepath2);
-  const file1 = fs.readFileSync(path1, 'utf8');
-  const file2 = fs.readFileSync(path2, 'utf8');
-  const json1 = JSON.parse(file1);
-  const json2 = JSON.parse(file2);
-  const arrJson1 = Object.keys(json1);
-  const arrJson2 = Object.keys(json2);
+
   const unionArr = _.union(arrJson1, arrJson2).sort();
   const resultArr = unionArr.reduce((acc, key) => {
     if (arrJson1.includes(key) && arrJson2.includes(key)) {
