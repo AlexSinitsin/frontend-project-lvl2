685d3e3e0bf99a785e071ad2b3ed384040ebb0bc
Alexander Sinitsin <workbigbox@yandex.ru>
2021-06-18 14:52:15 +0700

    test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,35 @@
 #!/usr/bin/env node
+import path from 'path';
+import * as fs from 'fs';
+import _ from 'lodash';
 import { Command } from 'commander/esm.mjs';
-import gendiff from '../src/main.js';
+
+const gendiff = (filepath1, filepath2) => {
+  const path1 = path.resolve(filepath1);
+  console.log(path1 + '&');
+
+  const unionArr = _.union(arrJson1, arrJson2).sort();
+  const resultArr = unionArr.reduce((acc, key) => {
+    if (arrJson1.includes(key) && arrJson2.includes(key)) {
+      if (json1[key] === json2[key]) {
+        acc.push(`    ${key}: ${json1[key]}`);
+        return acc;
+      }
+      acc.push(`  - ${key}: ${json1[key]}`);
+      acc.push(`  + ${key}: ${json2[key]}`);
+      return acc;
+    }
+    if (arrJson1.includes(key) && !arrJson2.includes(key)) {
+      acc.push(`  - ${key}: ${json1[key]}`);
+      return acc;
+    }
+    acc.push(`  + ${key}: ${json2[key]}`);
+    return acc;
+  }, []);
+  const result = `{\r\n${resultArr.join('\r\n')}\r\n}`;
+  console.log(result);
+  return result;
+};
 
 const program = new Command();
 
