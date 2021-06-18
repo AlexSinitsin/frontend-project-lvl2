37ef87795c4150b8685af721a25dc66629bddf24
Alexander Sinitsin <workbigbox@yandex.ru>
2021-06-18 14:40:09 +0700

    upload

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,11 @@
 import { expect, test } from '@jest/globals';
+import * as fs from 'fs';
 import gendiff from '../src/main.js';
 
 const file1 = new URL('../__fixtures__/file1.json', import.meta.url);
 const file2 = new URL('../__fixtures__/file2.json', import.meta.url);
-
-const result = '{\n  ' +
-    '- follow: false\n  ' +
-    '  host: hexlet.io\n  ' +
-    '- proxy: 123.234.53.22\n  ' +
-    '- timeout: 50\n  ' +
-    '+ timeout: 20\n  ' +
-    '+ verbose: true\n' +
-  '}';
+const file3 = new URL('../__fixtures__/result.txt', import.meta.url);
+const result = fs.readFileSync(file3, 'utf-8');
 
 test('getdiff', () => {
   expect(gendiff(file1.pathname, file2.pathname)).toEqual(result);
