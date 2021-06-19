d8cf7e03eab7ae26ca07f4df1cef1e8462a2109b
Alexander Sinitsin <workbigbox@yandex.ru>
2021-06-19 11:14:39 +0700

    add new funstions in program

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,11 +2,16 @@ import { expect, test } from '@jest/globals';
 import * as fs from 'fs';
 import gendiff from '../src/main.js';
 
-const file1 = new URL('../__fixtures__/file1.json', import.meta.url);
-const file2 = new URL('../__fixtures__/file2.json', import.meta.url);
-const file3 = new URL('../__fixtures__/result.txt', import.meta.url);
-const result = fs.readFileSync(file3, 'utf-8');
+const fileJson1 = new URL('../__fixtures__/file1.json', import.meta.url);
+const fileJson2 = new URL('../__fixtures__/file2.json', import.meta.url);
+
+const fileYaml1 = new URL('../__fixtures__/file1.yml', import.meta.url);
+const fileYaml2 = new URL('../__fixtures__/file2.yml', import.meta.url);
+
+const fileResult = new URL('../__fixtures__/result.txt', import.meta.url);
+const result = fs.readFileSync(fileResult, 'utf-8');
 
 test('getdiff', () => {
-  expect(gendiff(file1.pathname, file2.pathname)).toEqual(result);
+  expect(gendiff(fileJson1.pathname, fileJson2.pathname)).toEqual(result);
+  expect(gendiff(fileYaml1.pathname, fileYaml2.pathname)).toEqual(result);
 });
--- src/main.js
+++ src/main.js
@@ -1,32 +1,27 @@
-import path from 'path';
-import * as fs from 'fs';
 import _ from 'lodash';
+import parser from './parsers.js';
 
 const gendiff = (filepath1, filepath2) => {
-  const path1 = path.resolve(filepath1);
-  const path2 = path.resolve(filepath2);
-  const file1 = fs.readFileSync(path1, 'utf8');
-  const file2 = fs.readFileSync(path2, 'utf8');
-  const json1 = JSON.parse(file1);
-  const json2 = JSON.parse(file2);
-  const arrJson1 = Object.keys(json1);
-  const arrJson2 = Object.keys(json2);
-  const unionArr = _.union(arrJson1, arrJson2).sort();
+  const dataFile1 = parser(filepath1);
+  const dataFile2 = parser(filepath2);
+  const arrFile1 = Object.keys(dataFile1);
+  const arrFile2 = Object.keys(dataFile2);
+  const unionArr = _.union(arrFile1, arrFile2).sort();
   const resultArr = unionArr.reduce((acc, key) => {
-    if (arrJson1.includes(key) && arrJson2.includes(key)) {
-      if (json1[key] === json2[key]) {
-        acc.push(`    ${key}: ${json1[key]}`);
+    if (arrFile1.includes(key) && arrFile2.includes(key)) {
+      if (dataFile1[key] === dataFile2[key]) {
+        acc.push(`    ${key}: ${dataFile1[key]}`);
         return acc;
       }
-      acc.push(`  - ${key}: ${json1[key]}`);
-      acc.push(`  + ${key}: ${json2[key]}`);
+      acc.push(`  - ${key}: ${dataFile1[key]}`);
+      acc.push(`  + ${key}: ${dataFile2[key]}`);
       return acc;
     }
-    if (arrJson1.includes(key) && !arrJson2.includes(key)) {
-      acc.push(`  - ${key}: ${json1[key]}`);
+    if (arrFile1.includes(key) && !arrFile2.includes(key)) {
+      acc.push(`  - ${key}: ${dataFile1[key]}`);
       return acc;
     }
-    acc.push(`  + ${key}: ${json2[key]}`);
+    acc.push(`  + ${key}: ${dataFile2[key]}`);
     return acc;
   }, []);
   const result = `{\r\n${resultArr.join('\r\n')}\r\n}`;
@@ -35,5 +30,3 @@ const gendiff = (filepath1, filepath2) => {
 };
 
 export default gendiff;
-
-//sc
